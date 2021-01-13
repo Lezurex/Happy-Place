f855660c403c14baf44069ecafdcdfdbb57ffae6
Lenny Angst <lenny.angst@easyid.ch>
2021-01-13 09:35:22 +0100

    Add login page

---
+++ src/admin/index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Happy Place - Login</title>
+
+    <link rel="stylesheet" href="/css/admin.css">
+</head>
+<body>
+<div id="login-content">
+    <table>
+        <tr>
+            <td>Benutzername: </td>
+            <td><input type="text" id="login-user"></td>
+        </tr>
+        <tr>
+            <td>Passwort: </td>
+            <td><input type="password" id="login-password"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button id="login-btn">Anmelden</button></td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
