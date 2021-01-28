87c97e11f6df5ea09ee27d49b3b3a2bf2d58f537
Lezurex <lenny.angst@easyid.ch>
2021-01-28 13:51:43 +0100

    Migrate new OpenLayers Version to index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,19 @@
 <?php
 
-require $_SERVER['DOCUMENT_ROOT'] . '/php/database/DatabaseAdapter.php';
+use php\objects\Marker;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/php/database/DatabaseAdapter.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/php/objects/Marker.php';
 
 $db = new DatabaseAdapter();
+
+$markers = array();
+
+$data = $db->getAllStringsFromTable("markers");
+foreach ($data as $markerData) {
+    $marker = new Marker($markerData['id'], $markerData['lat'], $markerData['lon'], $markerData['city_id'], json_decode($markerData['student_ids']));
+    array_push($markers, $marker);
+}
 
 ?>
 
@@ -15,142 +26,186 @@ $db = new DatabaseAdapter();
     <meta http-equiv="content-style-type" content="text/css"/>
     <meta http-equiv="content-language" content="de"/>
     <meta name="author" content="Thomas Heiles"/>
-    <link rel="stylesheet" type="text/css" href="map.css"/>
-    <script type="text/javascript" src="http://www.openlayers.org/api/OpenLayers.js"></script>
-    <script type="text/javascript" src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>
-    <script type="text/javascript" src="tom.js"></script>
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css"
+          type="text/css">
+    <link rel="stylesheet" href="/css/new_styles.css">
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
 
-    <script type="text/javascript">
-        //<![CDATA[
-
-        var map;
-        var layer_mapnik;
-        var layer_tah;
-        var layer_markers;
-
-        function drawmap() {
-            // Popup und Popuptext mit evtl. Grafik
-
-            OpenLayers.Lang.setCode('de');
-
-            // Position und Zoomstufe der Karte
-            var lon = 8.520837;
-            var lat = 47.360117;
-            var zoom = 10;
-
-            map = new OpenLayers.Map('map', {
-                projection: new OpenLayers.Projection("EPSG:900913"),
-                displayProjection: new OpenLayers.Projection("EPSG:4326"),
-                controls: [
-                    new OpenLayers.Control.Navigation(),
-                    new OpenLayers.Control.LayerSwitcher(),
-                    new OpenLayers.Control.PanZoomBar()],
-                maxExtent:
-                    new OpenLayers.Bounds(-20037508.34, -20037508.34,
-                        20037508.34, 20037508.34),
-                numZoomLevels: 18,
-                maxResolution: 156543,
-                units: 'meters'
-            });
-
-            layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-            layer_markers = new OpenLayers.Layer.Markers("Address", {
-                projection: new OpenLayers.Projection("EPSG:4326"),
-                visibility: true, displayInLayerSwitcher: false
-            });
-
-            map.addLayers([layer_mapnik, layer_markers]);
-            jumpTo(lon, lat, zoom);
-
-            // Position des Markers
-            addMarker(layer_markers, 8.520837, 47.360117, "<p>ZLI Zürcher Lehrbetriebsverband</p>");
-            let markerJSON = JSON.parse(document.getElementById("marker-data").innerHTML);
-            markerJSON.forEach(element => {
-                let countString;
-                if (element.students.length === 1) {
-                    countString = "Eine Person lebt an diesem Ort:";
-                } else {
-                    countString = element.students.length + " Personen leben an diesem Ort:";
-                }
-                let listItems = "";
-                element.students.forEach(student => {
-                    listItems += "<li>" + student.first + " " + student.last + "</li>";
-                })
-
-                addMarker(layer_markers, element.lon, element.lat, "<strong>" + element.city + "</strong><br><span>" + element.plz + "<br>" + countString + "</span><ul>" + listItems + "</ul>");
-                console.log(element);
-            });
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Verdana, Arial, serif;
         }
 
-        //]]>
-    </script>
+        .map {
+            height: 100%;
+            width: 100%;
+        }
+
+        #credit {
+            position: absolute;
+            z-index: 100;
+            bottom: 0;
+            right: 0;
+            padding: .5rem;
+            background-color: rgba(255,255,255,0.7);
+        }
+
+        #credit a {
+            transition: .5s;
+            color: black;
+            text-decoration: none;
+        }
+        #credit a:hover {
+            transition: .5s;
+            color: #1f1f1f;
+            text-decoration: underline #1f1f1f solid;
+        }
+    </style>
 
 </head>
-<body onload="drawmap();">
-
-<div id="header">
-    <div id="content">Happy Place Karte</div>
-    <div id="osm">© <a href="http://www.openstreetmap.org">OpenStreetMap</a>
-        und <a href="http://www.openstreetmap.org/copyright">Mitwirkende</a>,
-        <a href="http://creativecommons.org/licenses/by-sa/2.0/deed.de">CC-BY-SA</a>
-    </div>
+<body>
+<div id="credit">
+    <a href="https://www.openstreetmap.org/copyright">&copy OpenStreetMap contributors</a>
 </div>
-<div id="map">
+<div id="map" class="map"></div>
+<div id="popup" class="ol-popup">
+    <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+    <div id="popup-content"></div>
 </div>
 
-<div style="display: none" id="marker-data">
-    <?php
-
-    $markers = $db->getAllStringsFromTable("markers");
-    $data = array();
-
-    foreach ($markers as $marker) {
-        $city_data = $db->getStringsFromTable("cities", new Key("id", $marker['city_id']));
-
-        $data_item = array();
-        $data_item['lon'] = doubleval($marker['lon']);
-        $data_item['lat'] = doubleval($marker['lat']);
-        $data_item['icon'] = $marker['icon'];
-        $data_item['plz'] = $city_data['plz'];
-        $data_item['city'] = $city_data['name'];
-
-        $student_ids = json_decode($marker['student_ids']);
-        $students = array();
-
-        foreach ($student_ids as $student_id) {
-            $student = $db->getStringsFromTable("students", new Key("id", $student_id));
-            $student_item = array(
-                "first" => censorString($student['firstname']),
-                "last" => censorString($student['lastname'])
-            );
-            array_push($students, $student_item);
+<script type="text/javascript">
+    const markerData = [<?php
+        foreach ($markers as $marker) {
+            echo $marker->toJSON();
         }
+        ?>];
 
-        $data_item['students'] = $students;
+    const features = [];
 
-        array_push($data, $data_item);
+    let zli = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([8.520840,47.360150])),
+        name: "Zürcher Lehrbetriebsverband ICT"
+    })
+    zli.setStyle(new ol.style.Style({
+        image: new ol.style.Icon({
+            anchor: [0.5,0.5],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'fraction',
+            src: './img/zli.jpg',
+            scale: 0.1
+        })
+    }))
+    features.push(zli);
+
+    for (let marker of markerData) {
+        let listContent = "";
+        let headerText = ""
+        if (marker.students.length === 1) {
+            headerText = `Hier lebt ${marker.students.length} Person:<br>`;
+        } else {
+            headerText = `Hier leben ${marker.students.length} Personen:<br>`;
+        }
+        marker.students.forEach(student => {
+            listContent += `<li>${student.firstname} ${student.lastname}</li>`;
+        })
+        features.push(new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([marker.lng, marker.lat])),
+            name: `<b>${marker.zip} ${marker.name}</b><br>${headerText}<ul>${listContent}<ul>`
+        }));
     }
 
-    echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    /**
+     * Elements that make up the popup.
+     */
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup-content');
+    var closer = document.getElementById('popup-closer');
 
-    function censorString($string)
-    {
-        $chars = preg_split('//', $string, -1, PREG_SPLIT_NO_EMPTY);
-        $return_string = "";
-        $count = 0;
-        foreach ($chars as $char) {
-            if ($count == 0) {
-                $return_string .= $char;
-            } else {
-                $return_string .= "*";
-            }
-            $count++;
+    let markers = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: features
+        }),
+        style: new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 1],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'fraction',
+                src: './img/marker.png',
+                scale: 0.05
+            })
+        })
+    })
+
+    /**
+     * Create an overlay to anchor the popup to the map.
+     */
+    var overlay = new ol.Overlay({
+        element: container,
+        autoPan: true,
+        autoPanAnimation: {
+            duration: 250
         }
-        return $return_string;
-    }
+    });
 
-    ?>
-</div>
+
+    let map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.XYZ({
+                    urls: ["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png", "http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png", "http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png", "http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
+                })
+            }),
+            new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    format: new ol.format.GeoJSON(),
+                    url: './data/countries.geojson'
+                })
+            }),
+            markers
+        ],
+        overlays: [
+            overlay
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([8.5208324, 47.360127]),
+            zoom: 10
+        })
+    });
+
+    /**
+     * Add a click handler to hide the popup.
+     * @return {boolean} Don't follow the href.
+     */
+    closer.onclick = function() {
+        overlay.setPosition(undefined);
+        closer.blur();
+        return false;
+    };
+
+    /**
+     * Add a click handler to the map to render the popup.
+     */
+    map.on('singleclick', function(evt) {
+        var name = map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+            return feature.get('name');
+        })
+        if (name) {
+            container.style.display="block";
+            var coordinate = evt.coordinate;
+            content.innerHTML = name;
+            overlay.setPosition(coordinate);
+        } else {
+            container.style.display="none";
+        }
+    });
+
+
+
+</script>
 
 </body>
 </html>
---
+++ src/index_old.php
@@ -0,0 +1,156 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/php/database/DatabaseAdapter.php';
+
+$db = new DatabaseAdapter();
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de-de">
+<head>
+    <title>Happy Place</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="content-script-type" content="text/javascript"/>
+    <meta http-equiv="content-style-type" content="text/css"/>
+    <meta http-equiv="content-language" content="de"/>
+    <meta name="author" content="Thomas Heiles"/>
+    <link rel="stylesheet" type="text/css" href="map.css"/>
+    <script type="text/javascript" src="http://www.openlayers.org/api/OpenLayers.js"></script>
+    <script type="text/javascript" src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>
+    <script type="text/javascript" src="tom.js"></script>
+
+    <script type="text/javascript">
+        //<![CDATA[
+
+        var map;
+        var layer_mapnik;
+        var layer_tah;
+        var layer_markers;
+
+        function drawmap() {
+            // Popup und Popuptext mit evtl. Grafik
+
+            OpenLayers.Lang.setCode('de');
+
+            // Position und Zoomstufe der Karte
+            var lon = 8.520837;
+            var lat = 47.360117;
+            var zoom = 10;
+
+            map = new OpenLayers.Map('map', {
+                projection: new OpenLayers.Projection("EPSG:900913"),
+                displayProjection: new OpenLayers.Projection("EPSG:4326"),
+                controls: [
+                    new OpenLayers.Control.Navigation(),
+                    new OpenLayers.Control.LayerSwitcher(),
+                    new OpenLayers.Control.PanZoomBar()],
+                maxExtent:
+                    new OpenLayers.Bounds(-20037508.34, -20037508.34,
+                        20037508.34, 20037508.34),
+                numZoomLevels: 18,
+                maxResolution: 156543,
+                units: 'meters'
+            });
+
+            layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+            layer_markers = new OpenLayers.Layer.Markers("Address", {
+                projection: new OpenLayers.Projection("EPSG:4326"),
+                visibility: true, displayInLayerSwitcher: false
+            });
+
+            map.addLayers([layer_mapnik, layer_markers]);
+            jumpTo(lon, lat, zoom);
+
+            // Position des Markers
+            addMarker(layer_markers, 8.520837, 47.360117, "<p>ZLI Zürcher Lehrbetriebsverband</p>");
+            let markerJSON = JSON.parse(document.getElementById("marker-data").innerHTML);
+            markerJSON.forEach(element => {
+                let countString;
+                if (element.students.length === 1) {
+                    countString = "Eine Person lebt an diesem Ort:";
+                } else {
+                    countString = element.students.length + " Personen leben an diesem Ort:";
+                }
+                let listItems = "";
+                element.students.forEach(student => {
+                    listItems += "<li>" + student.first + " " + student.last + "</li>";
+                })
+
+                addMarker(layer_markers, element.lon, element.lat, "<strong>" + element.city + "</strong><br><span>" + element.plz + "<br>" + countString + "</span><ul>" + listItems + "</ul>");
+                console.log(element);
+            });
+        }
+
+        //]]>
+    </script>
+
+</head>
+<body onload="drawmap();">
+
+<div id="header">
+    <div id="content">Happy Place Karte</div>
+    <div id="osm">© <a href="http://www.openstreetmap.org">OpenStreetMap</a>
+        und <a href="http://www.openstreetmap.org/copyright">Mitwirkende</a>,
+        <a href="http://creativecommons.org/licenses/by-sa/2.0/deed.de">CC-BY-SA</a>
+    </div>
+</div>
+<div id="map">
+</div>
+
+<div style="display: none" id="marker-data">
+    <?php
+
+    $markers = $db->getAllStringsFromTable("markers");
+    $data = array();
+
+    foreach ($markers as $marker) {
+        $city_data = $db->getStringsFromTable("cities", new Key("id", $marker['city_id']));
+
+        $data_item = array();
+        $data_item['lon'] = doubleval($marker['lon']);
+        $data_item['lat'] = doubleval($marker['lat']);
+        $data_item['icon'] = $marker['icon'];
+        $data_item['plz'] = $city_data['plz'];
+        $data_item['city'] = $city_data['name'];
+
+        $student_ids = json_decode($marker['student_ids']);
+        $students = array();
+
+        foreach ($student_ids as $student_id) {
+            $student = $db->getStringsFromTable("students", new Key("id", $student_id));
+            $student_item = array(
+                "first" => censorString($student['firstname']),
+                "last" => censorString($student['lastname'])
+            );
+            array_push($students, $student_item);
+        }
+
+        $data_item['students'] = $students;
+
+        array_push($data, $data_item);
+    }
+
+    echo json_encode($data, JSON_UNESCAPED_UNICODE);
+
+    function censorString($string)
+    {
+        $chars = preg_split('//', $string, -1, PREG_SPLIT_NO_EMPTY);
+        $return_string = "";
+        $count = 0;
+        foreach ($chars as $char) {
+            if ($count == 0) {
+                $return_string .= $char;
+            } else {
+                $return_string .= "*";
+            }
+            $count++;
+        }
+        return $return_string;
+    }
+
+    ?>
+</div>
+
+</body>
+</html>
